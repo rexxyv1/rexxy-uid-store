8be9203f81b9049482de5044b5a2b5ec79caceff
rexxyv1 <rexxyv1@gmail.com>
2026-07-14 06:19:50 +0700

    Remove 'Rp' prefix from harga values

--- data.js
+++ data.js
@@ -1,27 +1,27 @@
 const uidData = [
     {
         uid: "123456789",
-        harga: "Rp25.000",
+        harga: "25.000",
         status: "ready"
     },
     {
         uid: "987654321",
-        harga: "Rp40.000",
+        harga: "40.000",
         status: "sold"
     },
     {
         uid: "556677889",
-        harga: "Rp30.000",
+        harga: "30.000",
         status: "ready"
     },
     {
         uid: "112233445",
-        harga: "Rp50.000",
+        harga: "50.000",
         status: "ready"
     },
     {
         uid: "778899001",
-        harga: "Rp35.000",
+        harga: "35.000",
         status: "sold"
     }
 ];
