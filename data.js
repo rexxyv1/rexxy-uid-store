6d81154b3806ae293eca9b1b85650a9e5a4f330c
rexxyv1 <rexxyv1@gmail.com>
2026-07-14 08:42:48 +0700

    Add files via upload

--- data.js
+++ data.js
@@ -171,4 +171,112 @@ login: "Guest",
 status: "ready"
 },
 
+{
+uid: "16132222157",
+hargaAsli: 35000,
+harga: 25000,
+promo: true,
+login: "Guest",
+status: "ready"
+},
+
+{
+uid: "16132222286",
+hargaAsli: 35000,
+harga: 25000,
+promo: true,
+login: "Guest",
+status: "ready"
+},
+
+{
+uid: "16132222396",
+hargaAsli: 35000,
+harga: 25000,
+promo: true,
+login: "Guest",
+status: "ready"
+},
+
+{
+uid: "16132222644",
+hargaAsli: 35000,
+harga: 25000,
+promo: true,
+login: "Guest",
+status: "ready"
+},
+
+{
+uid: "16132222704",
+hargaAsli: 35000,
+harga: 25000,
+promo: true,
+login: "Guest",
+status: "ready"
+},
+
+{
+uid: "16132222922",
+hargaAsli: 35000,
+harga: 25000,
+promo: true,
+login: "Guest",
+status: "ready"
+},
+
+{
+uid: "16133110056",
+hargaAsli: 35000,
+harga: 25000,
+promo: true,
+login: "Guest",
+status: "ready"
+},
+
+{
+uid: "16133110076",
+hargaAsli: 35000,
+harga: 25000,
+promo: true,
+login: "Guest",
+status: "ready"
+},
+
+{
+uid: "16133111170",
+hargaAsli: 35000,
+harga: 25000,
+promo: true,
+login: "Guest",
+status: "ready"
+},
+
+{
+uid: "16133077774",
+hargaAsli: 35000,
+harga: 25000,
+promo: true,
+login: "Guest",
+status: "ready"
+},
+
+{
+uid: "16133300070",
+hargaAsli: 35000,
+harga: 25000,
+promo: true,
+login: "Guest",
+status: "sold"
+},
+
+{
+uid: "16133777770",
+hargaAsli: 35000,
+harga: 25000,
+promo: true,
+login: "Guest",
+status: "sold"
+},
+
 ];
