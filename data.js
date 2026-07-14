6ee1640834c64d6e7e64f3a87c0ad43e95b1250d
rexxyv1 <rexxyv1@gmail.com>
2026-07-14 08:28:22 +0700

    Add files via upload

--- data.js
+++ data.js
@@ -1,21 +1,291 @@
 const uidData = [
 
 {
-    uid: "123456789",
-    hargaAsli: 35000,
-    harga: 25000,
-    promo: true,
-    login: "Guest",
-    status: "ready"
+uid: "16132444469",
+hargaAsli: 35000,
+harga: 25000,
+promo: true,
+login: "Guest",
+status: "ready"
 },
 
 {
-    uid: "987654321",
-    hargaAsli: 40000,
-    harga: 40000,
-    promo: false,
-    login: "Google",
-    status: "sold"
+uid: "16132444476",
+hargaAsli: 35000,
+harga: 25000,
+promo: true,
+login: "Guest",
+status: "ready"
+},
+
+{
+uid: "16132225500",
+hargaAsli: 35000,
+harga: 25000,
+promo: true,
+login: "Guest",
+status: "ready"
+},
+
+{
+uid: "16132399998",
+hargaAsli: 35000,
+harga: 25000,
+promo: true,
+login: "Guest",
+status: "ready"
+},
+
+{
+uid: "16132488886",
+hargaAsli: 35000,
+harga: 25000,
+promo: true,
+login: "Guest",
+status: "ready"
+},
+
+{
+uid: "16132504444",
+hargaAsli: 35000,
+harga: 25000,
+promo: true,
+login: "Guest",
+status: "ready"
+},
+
+{
+uid: "16132567888",
+hargaAsli: 35000,
+harga: 25000,
+promo: true,
+login: "Guest",
+status: "ready"
+},
+
+{
+uid: "16132679000",
+hargaAsli: 35000,
+harga: 25000,
+promo: true,
+login: "Guest",
+status: "ready"
+},
+
+{
+uid: "16132750002",
+hargaAsli: 35000,
+harga: 25000,
+promo: true,
+login: "Guest",
+status: "ready"
+},
+
+{
+uid: "16132864444",
+hargaAsli: 35000,
+harga: 25000,
+promo: true,
+login: "Guest",
+status: "ready"
+},
+
+{
+uid: "16132883333",
+hargaAsli: 35000,
+harga: 25000,
+promo: true,
+login: "Guest",
+status: "ready"
+},
+
+{
+uid: "16132886666",
+hargaAsli: 35000,
+harga: 25000,
+promo: true,
+login: "Guest",
+status: "ready"
+},
+
+{
+uid: "16132886669",
+hargaAsli: 35000,
+harga: 25000,
+promo: true,
+login: "Guest",
+status: "ready"
+},
+
+{
+uid: "16132944448",
+hargaAsli: 35000,
+harga: 25000,
+promo: true,
+login: "Guest",
+status: "ready"
+},
+
+{
+uid: "16132962222",
+hargaAsli: 35000,
+harga: 25000,
+promo: true,
+login: "Guest",
+status: "ready"
+},
+
+{
+uid: "16132976666",
+hargaAsli: 35000,
+harga: 25000,
+promo: true,
+login: "Guest",
+status: "ready"
+},
+
+{
+uid: "16132997777",
+hargaAsli: 35000,
+harga: 25000,
+promo: true,
+login: "Guest",
+status: "ready"
+},
+
+{
+uid: "16132999941",
+hargaAsli: 35000,
+harga: 25000,
+promo: true,
+login: "Guest",
+status: "ready"
+},
+
+{
+uid: "16132222036",
+hargaAsli: 35000,
+harga: 25000,
+promo: true,
+login: "Guest",
+status: "ready"
+},
+
+{
+uid: "16132222157",
+hargaAsli: 35000,
+harga: 25000,
+promo: true,
+login: "Guest",
+status: "ready"
+},
+
+{
+uid: "16132222286",
+hargaAsli: 35000,
+harga: 25000,
+promo: true,
+login: "Guest",
+status: "ready"
+},
+
+{
+uid: "16132222396",
+hargaAsli: 35000,
+harga: 25000,
+promo: true,
+login: "Guest",
+status: "ready"
+},
+
+{
+uid: "16132222644",
+hargaAsli: 35000,
+harga: 25000,
+promo: true,
+login: "Guest",
+status: "ready"
+},
+
+{
+uid: "16132222704",
+hargaAsli: 35000,
+harga: 25000,
+promo: true,
+login: "Guest",
+status: "ready"
+},
+
+{
+uid: "16132222922",
+hargaAsli: 35000,
+harga: 25000,
+promo: true,
+login: "Guest",
+status: "ready"
+},
+
+{
+uid: "16133110056",
+hargaAsli: 35000,
+harga: 25000,
+promo: true,
+login: "Guest",
+status: "ready"
+},
+
+{
+uid: "16133110076",
+hargaAsli: 35000,
+harga: 25000,
+promo: true,
+login: "Guest",
+status: "ready"
+},
+
+{
+uid: "16133111170",
+hargaAsli: 35000,
+harga: 25000,
+promo: true,
+login: "Guest",
+status: "ready"
+},
+
+{
+uid: "16133077774",
+hargaAsli: 35000,
+harga: 25000,
+promo: true,
+login: "Guest",
+status: "ready"
+},
+
+{
+uid: "16133300070",
+hargaAsli: 35000,
+harga: 25000,
+promo: true,
+login: "Guest",
+status: "sold"
+},
+
+{
+uid: "16133777770",
+hargaAsli: 35000,
+harga: 25000,
+promo: true,
+login: "Guest",
+status: "sold"
+},
+
+{
+uid: "16137555558",
+hargaAsli: 35000,
+harga: 25000,
+promo: true,
+login: "Guest",
+status: "ready"
 }
 
 ];
