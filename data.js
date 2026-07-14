ef89b9244d77e798fa93b8ab255058ca66e582ec
rexxyv1 <rexxyv1@gmail.com>
2026-07-14 07:05:39 +0700

    Fix formatting and update harga and login fields

--- data.js
+++ data.js
@@ -1,32 +1,24 @@
 const uidData = [
+
     {
         uid: "123456789",
-        harga: "25000",
-        login: "Guest"
+        harga: 25000,
+        login: "Guest",
         status: "ready"
     },
+
     {
         uid: "987654321",
-        harga: "40000",
-        login: "Guest"
+        harga: 40000,
+        login: "Google",
         status: "sold"
     },
+
     {
         uid: "556677889",
-        harga: "30000",
-        login: "Guest"
+        harga: 30000,
+        login: "Facebook",
         status: "ready"
-    },
-    {
-        uid: "112233445",
-        harga: "50000",
-        login: "Guest"
-        status: "ready"
-    },
-    {
-        uid: "778899001",
-        harga: "35000",
-        login: "Guest"
-        status: "sold"
     }
+
 ];
