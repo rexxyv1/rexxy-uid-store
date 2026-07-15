d6acdfbeff2cffb640e9548a561ade3746b1cd5a
rexxyv1 <rexxyv1@gmail.com>
2026-07-16 02:46:57 +0700

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,18 @@
+// ==============================
+// Firebase Configuration
+// ==============================
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDp_p8Fbxr7gp-au1CXCGP-uSpGTEOGnlI",
+  authDomain: "rexxy-uid-store.firebaseapp.com",
+  projectId: "rexxy-uid-store",
+  storageBucket: "rexxy-uid-store.firebasestorage.app",
+  messagingSenderId: "285573461304",
+  appId: "1:285573461304:web:0a4f82ea78d1c8949b7dcd"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Firestore
+const db = firebase.firestore();
